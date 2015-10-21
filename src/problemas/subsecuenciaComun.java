aaf5e0d3b644612d3cb886406e4e72133c731b77
aam0093 <aam0093@alu.ubu.es>
2015-10-22 00:41:35 +0200

    1º versión acabada LCS.

--- src/problemas/subsecuenciaComun.java
+++ src/problemas/subsecuenciaComun.java
@@ -5,6 +5,9 @@ public class subsecuenciaComun implements Problemas{
 	private String cadena1 ="";
 	private String cadena2 ="";
 	String res = "";
+	int maxLength = -1;
+	int[][] matriz;
+	
 	
 	public subsecuenciaComun (String c1 , String c2){
 		cadena1 = c1;
@@ -21,61 +24,74 @@ public class subsecuenciaComun implements Problemas{
 //		
 //	}
 	
+	public int getMaxLenght(){
+		return (maxLength > 0)? maxLength : null;  
+	}
+	
 	public String getLCS(){
      
         int n = cadena1.length();
         int m = cadena2.length();
-        int[][] C = new int[n+1][m+1];
-        int[][] B = new int[n+1][m+1];
+        matriz = new int[n+1][m+1];
+        int[][] aux = new int[n+1][m+1];
 	
-        /* C[i][0] = 0 for 0<=i<=n */
-        for (int i = 0; i <= n; i++) {
-            C[i][0] = 0;
-        }
-	
-        /* C[0][j] = 0 for  0<=j<=m */
-        for (int j = 0; j <= m; j++) {
-            C[0][j] = 0;
-        }
+        initialize(n, m);
         
         /* dynamic programming */
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (cadena1.charAt(i-1) == cadena2.charAt(j-1)) {
-                    C[i][j]=C[i-1][j-1]+1;
-                    B[i][j]=1;  /* diagonal */
+                    matriz[i][j]=matriz[i-1][j-1]+1;
+                    aux[i][j]=1;  /* diagonal */
                 }
-                else if (C[i-1][j]>=C[i][j-1]) {
-                    C[i][j]=C[i-1][j];
-                    B[i][j] = 2;  /* down */
+                else if (matriz[i-1][j]>=matriz[i][j-1]) {
+                    matriz[i][j]=matriz[i-1][j];
+                    aux[i][j] = 2;  /* down */
                 }
                 else {
-                    C[i][j]=C[i][j-1];     
-                    B[i][j]=3;   /* forword */
+                    matriz[i][j]=matriz[i][j-1];     
+                    aux[i][j]=3;   /* right */
                 }
             }
         }
+        maxLength = matriz[n][m];
         /* Backtracking */
         String lcs = new String();
         int i=n;
         int  j=m;
         while (i!=0 && j!=0) {
-            if (B[i][j] ==1) {   /* diagonal */
+            if (aux[i][j] ==1) {   /* diagonal */
                 lcs =cadena1.charAt(i-1) + lcs;
                 i = i - 1;
                 j = j - 1;
             }
-            if (B[i][j] == 2) {  /* up */
+            if (aux[i][j] == 2) {  /* up */
                 i = i - 1;
             }
-            if (B[i][j] == 3) {  /* backword */
+            if (aux[i][j] == 3) {  /* left */
                 j = j - 1;
             }
         }
         
         return lcs;
 
-	}  
+	}
+/**
+ * @param n
+ * @param m
+ * @param matriz
+ */
+private void initialize(int n, int m) {
+	/* C[i][0] = 0 for 0<=i<=n */
+	for (int i = 0; i <= n; i++) {
+	    matriz[i][0] = 0;
+	}
+
+	/* C[0][j] = 0 for  0<=j<=m */
+	for (int j = 0; j <= m; j++) {
+	    matriz[0][j] = 0;
+	}
+}  
 
 	@Override
 	public String getResult() {
