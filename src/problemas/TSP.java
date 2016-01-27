0c1c94aecd197c126f175f0a9448b9e4afe378ed
aam0093 <aam0093@alu.ubu.es>
2016-01-27 01:13:41 +0100

    versión 1.2 Problema del viajante #17

--- src/problemas/TSP.java
+++ src/problemas/TSP.java
@@ -9,86 +9,94 @@ import java.util.*;
  * @author Asier Alonso Morante
  *
  */
-public class TSP implements Problema{ 
+public class TSP implements Problema {
 	private static int[][] dist;
 	private static List<Integer> path = new ArrayList<Integer>();
 	private int nodes;
 	private int maxDist;
-	private int start ;	
-	
-	public TSP(int n){
+	private int start;
+
+	public TSP(int n) {
 		nodes = n;
 		dist = new int[nodes][nodes];
 	}
-	
-	public void startNode(int n){
+
+	public void startNode(int n) {
 		int start = 0;
 	}
-	
-	public void initialize(int maxDist ){
-		for (int i =0 ; i< dist.length; i++){
-			for (int j=0; j< dist.length; j++){
-				if ( i == j){
-					dist[i][j] = -1; 
-				}else{ 
-					dist[i][j] = (int) (Math.random() * maxDist); 
+
+	public void initialize(int maxDist) {
+		for (int i = 0; i < dist.length; i++) {
+			for (int j = 0; j < dist.length; j++) {
+				if (i == j) {
+					dist[i][j] = -1;
+				} else {
+					dist[i][j] = (int) (Math.random() * maxDist);
 				}
 			}
 		}
 	}
-	public int getDistance (int n,int sta){
+
+	public int[][] getMatriz() {
+		return dist;
+	}
+
+	public int getNumNodos() {
+		return nodes;
+	}
+
+	public int getDistance(int n, int sta) {
 		int distancia = 0;
 		distancia = dist[sta][n];
 		return distancia;
-		}
-	
-	public List<Integer>getPath(){
+	}
+
+	public List<Integer> getPath() {
 		return path;
 	}
-	
-	public int[][] getDist(){
+
+	public int[][] getDist() {
 		return dist;
 	}
-			
+
 	public void resolve() {
 		List<Integer> s = new ArrayList<Integer>();
-		int aux=-5;
+		int aux = -5;
 		int next = start;
-		//	int startN = 1;
-		while(s.size() < nodes){
-			int distance = Integer.MAX_VALUE; 
-			//	if (i != startNode){
-			for (int i = 0; i < nodes;i++){
-				if (next != i && !path.contains(i+1)){
-					if (getDistance(i,next) < distance){
-						distance = getDistance(i,next); //}
-						aux = i; 
+		// int startN = 1;
+		while (s.size() < nodes) {
+			int distance = Integer.MAX_VALUE;
+			// if (i != startNode){
+			for (int i = 0; i < nodes; i++) {
+				if (next != i && !path.contains(i + 1)) {
+					if (getDistance(i, next) < distance) {
+						distance = getDistance(i, next); // }
+						aux = i;
 					}
 				}
 			}
 			s.add(aux);
-			path.add(aux+1);
+			path.add(aux + 1);
 			next = aux;
 		}
-		
+
 	}
-	
-	public static void main(String[]args){
+
+	public static void main(String[] args) {
 		TSP viajante = new TSP(4);
 		viajante.startNode(0);
 		viajante.initialize(10);
-		for (int [] fila : dist  ){
+		for (int[] fila : dist) {
 			System.out.println("");
-			for (int el : fila){
+			for (int el : fila) {
 				System.out.print(el + " ");
 			}
-			
+
 		}
 		System.out.println("");
 		viajante.resolve();
 		System.out.println("Camino minimo : " + viajante.getPath());
-		
-	
+
 	}
 
 	public String execute() {
@@ -100,5 +108,23 @@ public class TSP implements Problema{
 	public String getTipo() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public Problema recuperarProblema(String semilla) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getTipoPregunta() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setTipoPregunta(int tipoPregunta) {
+		// TODO Auto-generated method stub
+
 	}
 }
