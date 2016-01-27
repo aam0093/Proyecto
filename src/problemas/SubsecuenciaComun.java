249719c943018daaf99eff86c51a46f611cb7799
aam0093 <aam0093@alu.ubu.es>
2016-01-27 01:12:14 +0100

    versión 1.2 Subsecuencia Comun Mas Larga #14

--- src/problemas/SubsecuenciaComun.java
+++ src/problemas/SubsecuenciaComun.java
@@ -8,35 +8,41 @@
  * @version 1.2, 18/11/2015
  */
 
-
 package problemas;
 
 import java.util.Random;
 
+import pregunta.Semilla;
+
 public class SubsecuenciaComun implements Problema {
 
-	 /** Primera cadena objeto de la comparación */
+	/** Primera cadena objeto de la comparación */
 	private String cadena1 = "";
-	
-	 /** Segunda cadena objeto de la comparación */
+
+	/** Segunda cadena objeto de la comparación */
 	private String cadena2 = "";
-	
-	 /** Almacena el resultado del problema */
+
+	/** Almacena el resultado del problema */
 	private String res = new String();
-	
-	 /** Almacena la longitud maxima de la cadena resultado. Inicializa a -1 */
+
+	/** Almacena la longitud maxima de la cadena resultado. Inicializa a -1 */
 	int maxLength = -1;
-	
-	 /** Matriz que resuelve el problema */
+
+	/** Matriz que resuelve el problema */
 	int[][] matriz;
-	
+
+	long semilla = 0;
+	int tipodePregunta;
+
 	/**
-     * Crea el problema con las longitudes introducidas como parametros
-     * 
-     * @param longitud1 entero que indica la longitud deseada en la cadena 1
-     * @param longitud2 entero que indica la longitud deseada en la cadena 2
-     * @throws Exception 
-     */
+	 * Crea el problema con las longitudes introducidas como parametros
+	 * 
+	 * @param longitud1
+	 *            entero que indica la longitud deseada en la cadena 1
+	 * @param longitud2
+	 *            entero que indica la longitud deseada en la cadena 2
+	 * @throws Exception
+	 */
 	public SubsecuenciaComun(int longitud1, int longitud2) {
 		Random rd = new Random();
 		int i = 0;
@@ -51,6 +57,8 @@ public class SubsecuenciaComun implements Problema {
 			cadena2 += c;
 			j++;
 		}
+		Semilla seed = new Semilla(cadena1, cadena2, "lcs");
+		semilla = seed.getSeed();
 	}
 
 	public boolean isValid(String c) {
@@ -62,7 +70,7 @@ public class SubsecuenciaComun implements Problema {
 		return cadena1;
 	}
 
-	/** Obtiene la cadena 2 */	
+	/** Obtiene la cadena 2 */
 	public String getCadena2() {
 		return cadena2;
 	}
@@ -71,29 +79,28 @@ public class SubsecuenciaComun implements Problema {
 	public int getMaxLenght() {
 		return (maxLength > 0) ? maxLength : null;
 	}
-	
+
 	/** Obtiene el resultado */
 	public String getResult() {
 		return res;
 	}
 
-	/** Obtiene la matriz*/
+	/** Obtiene la matriz */
 	public int[][] getMatriz() {
 		return matriz;
 	}
 
-	/** Obtiene el tipo de problema que es*/
+	/** Obtiene el tipo de problema que es */
 	@Override
 	public String getTipo() {
-		return TIPO.SUBSECUENCIA.toString();
+		return "SUBSECUENCIA";
 	}
-	
-	
+
 	/**
-     * Ejecuta la resolución del problema con los datos obtenidos.
-     * 
-     * @throws 
-     */
+	 * Ejecuta la resolución del problema con los datos obtenidos.
+	 * 
+	 * @throws
+	 */
 	@Override
 	public String execute() {
 		int n = cadena1.length();
@@ -103,36 +110,39 @@ public class SubsecuenciaComun implements Problema {
 
 		initialize(n, m);
 
-		//Rellena la matriz 
+		// Rellena la matriz
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= m; j++) {
 				if (cadena1.charAt(i - 1) == cadena2.charAt(j - 1)) {
 					matriz[i][j] = matriz[i - 1][j - 1] + 1;
-					aux[i][j] = 1; //1 los valores son iguales
+					aux[i][j] = 1; // 1 los valores son iguales
 				} else if (matriz[i - 1][j] >= matriz[i][j - 1]) {
 					matriz[i][j] = matriz[i - 1][j];
-					aux[i][j] = 2; //toma el valor del elemento superior en la matriz
+					aux[i][j] = 2; // toma el valor del elemento superior en la
+									// matriz
 				} else {
 					matriz[i][j] = matriz[i][j - 1];
-					aux[i][j] = 3; //toma el valor del elemento anterior en la matriz
+					aux[i][j] = 3; // toma el valor del elemento anterior en la
+									// matriz
 				}
 			}
 		}
 		maxLength = matriz[n][m];
-		
-		//Lectura de la matriz
+
+		// Lectura de la matriz
 		int i = n;
 		int j = m;
 		while (i != 0 && j != 0) {
-			if (aux[i][j] == 1) { //Coincidencia de caracteres, continua en diagonal
+			if (aux[i][j] == 1) { // Coincidencia de caracteres, continua en
+									// diagonal
 				res = cadena1.charAt(i - 1) + res;
 				i = i - 1;
 				j = j - 1;
 			}
-			if (aux[i][j] == 2) { //Continúa hacia arriba
+			if (aux[i][j] == 2) { // Continúa hacia arriba
 				i = i - 1;
 			}
-			if (aux[i][j] == 3) { //Continúa hacia la izquierda
+			if (aux[i][j] == 3) { // Continúa hacia la izquierda
 				j = j - 1;
 			}
 		}
@@ -141,8 +151,11 @@ public class SubsecuenciaComun implements Problema {
 
 	/**
 	 * Inicializa con ceros la matriz
-	 * @param n tamaño de la cadena 1
-	 * @param m tamaño de la cadena 2
+	 * 
+	 * @param n
+	 *            tamaño de la cadena 1
+	 * @param m
+	 *            tamaño de la cadena 2
 	 */
 	private void initialize(int n, int m) {
 		for (int i = 0; i <= n; i++) {
@@ -154,5 +167,23 @@ public class SubsecuenciaComun implements Problema {
 		}
 	}
 
+	@Override
+	public Problema recuperarProblema(String semilla) {
+		return null;
+	}
+
+	public long getSemilla() {
+		return semilla;
+	}
+
+	@Override
+	public int getTipoPregunta() {
+		return tipodePregunta;
+	}
+
+	@Override
+	public void setTipoPregunta(int tipoPregunta) {
+		this.tipodePregunta = tipoPregunta;
+	}
 
 }
